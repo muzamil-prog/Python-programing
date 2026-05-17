54d5d0d484ba27f3a8c8172a6e6d534492856b98
Muzammil Hussain <muzammil.org73@gmail.com>
2026-05-17 23:32:11 +0500

    Add files via upload

---
+++ chapter1.py
@@ -0,0 +1,17 @@
+# problem 1 print poem tunkle little star
+#problem 2 use repl make calculater
+# problem 3 use external module pyttsx3  
+# problem 4 write a python program to print the contents of the directory using the os module now yes we will do this with chat gpt 
+#python is a casesansitve language 
+
+# when are mulple line to print then use triple qute
+print('''Twinkle, Twinkle, Little Star
+
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+Twinkle, twinkle, little star,
+How I wonder what you are!
+''')
