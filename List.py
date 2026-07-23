e7f839376a8071bdc8aee0a484cac2f8de79e7e0
Muzammil Hussain <muzammil.org73@gmail.com>
2026-07-23 22:34:04 +0500

    Add files via upload

---
+++ List.py
@@ -0,0 +1,9 @@
+marks =[25, 44, 20, 44, 69,90]
+print("the highst marks of the student ")
+max(marks)
+print("the lowest the marks of the student")
+min(marks)
+print("the sum of all student marks mean total marks of all students")
+sum(marks)
+print("the average marks of the students")
+average=sum(marks)/len(marks);
