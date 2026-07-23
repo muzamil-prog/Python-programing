3d1bf30dbb68aa3e76a45bdb2f0ef2559c863542
Muzammil Hussain <muzammil.org73@gmail.com>
2026-07-23 22:35:27 +0500

    Add files via upload

---
+++ List2.py
@@ -0,0 +1,13 @@
+#prit the marks of students which are greater of 70
+marks =[25, 44, 20, 44, 69,90]
+print("the highst marks of the student ")
+max(marks)
+print("the lowest the marks of the student")
+min(marks)
+print("the sum of all student marks mean total marks of all students")
+sum(marks)
+print("the average marks of the students")
+average=sum(marks)/len(marks);
+if marks>70:
+    print("these are marks which are greate of 70 ")
+    print(marks)
