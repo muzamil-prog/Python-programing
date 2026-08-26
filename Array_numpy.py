36785207091e26f8261f278f7e8666692fab5c36
Muzammil Hussain <muzammil.org73@gmail.com>
2026-08-26 23:32:38 +0500

    Add files via upload

---
+++ Array_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create NumPy array
+marks = np.array([78, 85, 92, 67, 88])
+
+# Display original array
+print("Original Array:")
+print(marks)
+
+# Convert array to float
+marks_float = marks.astype(float)
+
+# Display converted array
+print("\nConverted Array:")
+print(marks_float)
