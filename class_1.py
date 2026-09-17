b1e8386acc32983ff3d375038eb58f194173fec5
Muzammil Hussain <muzammil.org73@gmail.com>
2026-09-17 08:45:32 -0700

    Add files via upload

---
+++ class_1.py
@@ -0,0 +1,9 @@
+a=5
+b=7
+print('sum', a+b)
+print('sub', a-b)
+print('mul', a*b)
+print('absoulte dision',a//b)
+print ('dision', a/b)
+c=int(input(print('plese etner the number')))
+print('sum', c+b)
