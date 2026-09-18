35861bd6a62571a868c90be62a8115d549124001
Muzammil Hussain <muzammil.org73@gmail.com>
2026-09-18 11:28:13 -0700

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,14 @@
+#arithmatic operator
+# logical operator
+# camprison operator
+a=7
+b=8
+if(a>b):
+    print('a is greater then b', a)
+else:
+    print('b is greater then a', b)
+if(a>=b and b<=a):
+        print('a is greate then b or both value are same')
+else:
+    print('b is greate then a')
+        
