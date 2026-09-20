4be0ebb6283ec08991eaea27ad03bb67cbf5aa24
Muzammil Hussain <muzammil.org73@gmail.com>
2026-09-20 10:07:55 -0700

    Add files via upload

---
+++ loop_problem.py
@@ -0,0 +1,10 @@
+for i in range(10):
+    print(i)
+print("Print all Even number from 1 to 50")
+print("Print all odd number from 1 to 50")
+
+for i in range(50):
+    if(i%2==0):
+        print(i)
+    else:
+        print(i)        
