698a83109a2a36fc45a3d487191623f3bacc6b58
Muzammil Hussain <muzammil.org73@gmail.com>
2026-09-22 11:41:38 -0700

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+text="python"
+reverse=""
+for i in text:
+    reverse=i+reverse
+print(reverse)
+print("Solve this problem with ::-1")
+string="programming"
+backward=string[::-1]  
+print(backward)  
